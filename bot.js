c2cd6df13b673f281678e36c775bf8bc65e0dfb3
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-30 15:10:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("1help",{type: 'LISTENING'})
+   client.user.setActivity("1help",{type: 'https://www.twitch.tv/idk'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
